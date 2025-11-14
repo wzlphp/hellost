841bcaa3d7c1eb5d38d234f35b0409478d9383b4
weizhanglong <wzlphp@gmail.com>
2025-11-14 14:02:34 +0800

    update

--- main.py
+++ main.py
@@ -52,3 +52,11 @@ start_time = st.slider(
      value=datetime(2020, 1, 1, 9, 30),
      format="MM/DD/YY - hh:mm")
 st.write("Start time:", start_time)
+
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
