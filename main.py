4762c5f1e07ea2768b4c20b0c9257b974b03a268
weizhanglong <wzlphp@gmail.com>
2025-11-14 13:56:51 +0800

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ else:
     st.write("按钮未被点击")
 
 df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
+     'first column': [1, 2, 3, 4, 5],
+     'second column': [10, 20, 30, 40, 50]
      })
 st.write(df)
