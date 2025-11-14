73631dd2bab48b83258260f67e9095212125e16e
weizhanglong <wzlphp@gmail.com>
2025-11-14 15:54:57 +0800

    update

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
 import streamlit as st
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would :blue[you] :red[like] to be contacted?:sunglasses:',
-    ('Email', 'Home phone', 'Mobile phone')
-)
 
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
-
-st.badge('这是一个错误信息')
-st.code('''
-import streamlit as st
-import pandas as pd
-from datetime import time, datetime
-''')
-
-
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-st.header("This is a header with a divider", divider="gray")
-st.header("These headers have rotating dividers", divider=True, anchor=False)
-st.header("One", divider=True)
-st.header("Two", divider=False)
-st.header("Three这里很宽很宽哦", divider=True, width=10)
-st.header("Four", divider=True, help="这是一个帮助信息")
-
+menus = st.sidebar.title(":blue[Chat]:red[Bot]")
 
