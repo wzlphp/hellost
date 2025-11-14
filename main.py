f1fed76afe7041601d53ec521dca465aee50820d
weizhanglong <wzlphp@gmail.com>
2025-11-14 14:10:44 +0800

    update

--- main.py
+++ main.py
@@ -60,3 +60,10 @@ options = st.multiselect(
      ['Yellow', 'Red'])
 
 st.write('You selected:', options)
+
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
