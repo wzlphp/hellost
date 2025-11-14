51399e397d9d283952e788c01dab3369fc0a2dac
weizhanglong <wzlphp@gmail.com>
2025-11-14 15:34:21 +0800

    update

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ add_slider = st.sidebar.slider(
     'Select a range of values',
     0.0, 100.0, (25.0, 75.0)
 )
+
+st.badger('这是一个错误信息')
+st.code('''
+import streamlit as st
+import pandas as pd
+from datetime import time, datetime
+''')
