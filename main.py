f2584eed561c6c21952a458ae7fb7ba8f777e73e
weizhanglong <wzlphp@gmail.com>
2025-11-14 15:29:17 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 # Add a selectbox to the sidebar:
 add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
+    'How would you like to be contacted?:sunglasses:',
     ('Email', 'Home phone', 'Mobile phone')
 )
 
