df257756105422c8bf357829e8875f023f1c1c70
weizhanglong <wzlphp@gmail.com>
2025-11-14 16:06:33 +0800

    update

--- main.py
+++ main.py
@@ -4,7 +4,4 @@ menus = st.sidebar.title(":blue[Chat]:red[Bot]")
 
 
 
-prompt = st.chat_input("请输入您的问题")
-
-if prompt:
-    st.write(f"您输入的问题是：{prompt}")
+prompt = st.chat_input("请输入您的问题", key="prompt", max_chars=50, accept_file=False, on_submit=lambda: st.write(f"您输入的问题是：{prompt}"))
