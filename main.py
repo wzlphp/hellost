31cbaeda13692c0b15c5ba70f82eac6e73cab9a8
weizhanglong <wzlphp@gmail.com>
2025-11-14 14:27:49 +0800

    update

--- main.py
+++ main.py
@@ -1,91 +1,128 @@
-import streamlit as st
-import pandas as pd
-from datetime import time, datetime
+# import streamlit as st
+# import pandas as pd
+# from datetime import time, datetime
 
 
-st.title("RAG 问答系统")
+# st.title("RAG 问答系统")
 
 
-st.header("st.*button*:sunglasses:")
+# st.header("st.*button*:sunglasses:")
 
-if st.button("点击我"):
-    st.write("按钮被点击了！")
-else:
-    st.write("按钮未被点击")
+# if st.button("点击我"):
+#     st.write("按钮被点击了！")
+# else:
+#     st.write("按钮未被点击")
 
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4, 5],
-     'second column': [10, 20, 30, 40, 50]
-     })
-st.write(df)
-
-
-st.subheader('Slider')
-
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
-
-# 样例 2
-
-st.subheader('Range slider')
-
-values = st.slider(
-     'Select a range of values',
-     0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
-
-# 样例 3
-
-st.subheader('Range time slider')
-
-appointment = st.slider(
-     "Schedule your appointment:",
-     value=(time(11, 30), time(12, 45)))
-st.write("You're scheduled for:", appointment)
-
-# 样例 4
-
-st.subheader('Datetime slider')
-
-start_time = st.slider(
-     "When do you start?",
-     value=datetime(2020, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
-st.write("Start time:", start_time)
+# df = pd.DataFrame({
+#      'first column': [1, 2, 3, 4, 5],
+#      'second column': [10, 20, 30, 40, 50]
+#      })
+# st.write(df)
 
 
-options = st.multiselect(
-     'What are your favorite colors',
-     ['Green', 'Yellow', 'Red', 'Blue'],
-     ['Yellow', 'Red'])
+# st.subheader('Slider')
 
-st.write('You selected:', options)
+# age = st.slider('How old are you?', 0, 130, 25)
+# st.write("I'm ", age, 'years old')
+
+# # 样例 2
+
+# st.subheader('Range slider')
+
+# values = st.slider(
+#      'Select a range of values',
+#      0.0, 100.0, (25.0, 75.0))
+# st.write('Values:', values)
+
+# # 样例 3
+
+# st.subheader('Range time slider')
+
+# appointment = st.slider(
+#      "Schedule your appointment:",
+#      value=(time(11, 30), time(12, 45)))
+# st.write("You're scheduled for:", appointment)
+
+# # 样例 4
+
+# st.subheader('Datetime slider')
+
+# start_time = st.slider(
+#      "When do you start?",
+#      value=datetime(2020, 1, 1, 9, 30),
+#      format="MM/DD/YY - hh:mm")
+# st.write("Start time:", start_time)
 
 
-st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
+# options = st.multiselect(
+#      'What are your favorite colors',
+#      ['Green', 'Yellow', 'Red', 'Blue'],
+#      ['Yellow', 'Red'])
+
+# st.write('You selected:', options)
+
+
+# st.latex(r'''
+#      a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+#      \sum_{k=0}^{n-1} ar^k =
+#      a \left(\frac{1-r^{n}}{1-r}\right)
+#      ''')
 
 
 
      
 
-st.write('Contents of the `.config.toml` file of this app')
+# st.write('Contents of the `.config.toml` file of this app')
 
-st.code("""
-[theme]
-primaryColor="#F39C12"
-backgroundColor="#2E86C1"
-secondaryBackgroundColor="#AED6F1"
-textColor="#FFFFFF"
-font="monospace"
-""")
+# st.code("""
+# [theme]
+# primaryColor="#F39C12"
+# backgroundColor="#2E86C1"
+# secondaryBackgroundColor="#AED6F1"
+# textColor="#FFFFFF"
+# font="monospace"
+# """)
 
-number = st.sidebar.slider('Select a number:', 0, 10, 5)
-st.write('Selected number from slider widget is:', number)
+# number = st.sidebar.slider('Select a number:', 0, 10, 5)
+# st.write('Selected number from slider widget is:', number)
 
 
-st.write(st.secrets['username'])
-st.write(st.secrets['password'])
+# st.write(st.secrets['username'])
+# st.write(st.secrets['password'])
+
+
+
+st.set_page_config(layout="wide")
+
+st.title('How to layout your Streamlit app')
+
+with st.expander('About this app'):
+  st.write('This app shows the various ways on how you can layout your Streamlit app.')
+  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+
+st.sidebar.header('Input')
+user_name = st.sidebar.text_input('What is your name?')
+user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+
+st.header('Output')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  if user_name != '':
+    st.write(f'👋 Hello {user_name}!')
+  else:
+    st.write('👈  Please enter your **name**!')
+
+with col2:
+  if user_emoji != '':
+    st.write(f'{user_emoji} is your favorite **emoji**!')
+  else:
+    st.write('👈 Please choose an **emoji**!')
+
+with col3:
+  if user_food != '':
+    st.write(f'🍴 **{user_food}** is your favorite **food**!')
+  else:
+    st.write('👈 Please choose your favorite **food**!')
