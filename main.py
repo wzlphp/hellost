b7a6b4d7e67b179d760c1f288ab1a8dac3dae2b9
weizhanglong <wzlphp@gmail.com>
2025-11-14 16:07:23 +0800

    update

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import streamlit as st
 menus = st.sidebar.title(":blue[Chat]:red[Bot]")
 
 
+def handle_prompt(prompt):
+    st.write(f"您输入的问题是：{prompt}")
 
-prompt = st.chat_input("请输入您的问题", key="prompt", max_chars=50, accept_file=False, on_submit=lambda: st.write(f"您输入的问题是：{prompt}"))
+
+prompt = st.chat_input("请输入您的问题", key="prompt", max_chars=50, accept_file=False, on_submit=handle_prompt)
