c2f7fc2c46fff1ae76c284f074702fb87551c89a
weizhanglong <wzlphp@gmail.com>
2025-11-14 14:43:57 +0800

    update

--- main.py
+++ main.py
@@ -90,37 +90,69 @@ from datetime import time, datetime
 # st.write(st.secrets['username'])
 # st.write(st.secrets['password'])
 
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
-st.title('How to layout your Streamlit app')
+# st.title('How to layout your Streamlit app')
 
-with st.expander('About this app'):
-  st.write('This app shows the various ways on how you can layout your Streamlit app.')
-  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+# with st.expander('About this app'):
+#   st.write('This app shows the various ways on how you can layout your Streamlit app.')
+#   st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+
+# st.sidebar.header('Input')
+# user_name = st.sidebar.text_input('What is your name?')
+# user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+# user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+
+# st.header('Output')
+
+# col1, col2, col3 = st.columns(3)
+
+# with col1:
+#   if user_name != '':
+#     st.write(f'👋 Hello {user_name}!')
+#   else:
+#     st.write('👈  Please enter your **name**!')
+
+# with col2:
+#   if user_emoji != '':
+#     st.write(f'{user_emoji} is your favorite **emoji**!')
+#   else:
+#     st.write('👈 Please choose an **emoji**!')
+
+# with col3:
+#   if user_food != '':
+#     st.write(f'🍴 **{user_food}** is your favorite **food**!')
+#   else:
+#     st.write('👈 Please choose your favorite **food**!')
+
+
+
+import requests
+
+st.title('🏀 Bored API app')
 
 st.sidebar.header('Input')
-user_name = st.sidebar.text_input('What is your name?')
-user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
-user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
 
-st.header('Output')
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
+
+c1, c2 = st.columns(2)
+with c1:
+  with st.expander('About this app'):
+    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+with c2:
+  with st.expander('JSON data'):
+    st.write(suggested_activity)
+
+st.header('Suggested activity')
+st.info(suggested_activity['activity'])
 
 col1, col2, col3 = st.columns(3)
-
 with col1:
-  if user_name != '':
-    st.write(f'👋 Hello {user_name}!')
-  else:
-    st.write('👈  Please enter your **name**!')
-
+  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
 with col2:
-  if user_emoji != '':
-    st.write(f'{user_emoji} is your favorite **emoji**!')
-  else:
-    st.write('👈 Please choose an **emoji**!')
-
+  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
 with col3:
-  if user_food != '':
-    st.write(f'🍴 **{user_food}** is your favorite **food**!')
-  else:
-    st.write('👈 Please choose your favorite **food**!')
+  st.metric(label='Price', value=suggested_activity['price'], delta='')
